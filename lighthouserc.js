20ad77b8635a03f9f4fdb6131ba724d3e1dc58ae
Abraham Williams <4braham@gmail.com>
2023-02-24 18:41:57 -0600

    Switch to @lhci/cli

---
+++ lighthouserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:5000/'],
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'unused-javascript': 'warn',
+        'csp-xss': 'off',
+        'font-display': 'off',
+        'maskable-icon': 'off',
+        'non-composited-animations': 'off',
+        'errors-in-console': 'off',
+        'categories:performance': ['error', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 1 }],
+        'categories:pwa': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
